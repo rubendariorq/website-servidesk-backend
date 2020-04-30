f3ca77024e46896595cfabef8a793b017f0ef629
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-04-29 20:22:14 -0500

    refactor: Refactor de code

--- src/controllers/dependencie.controller.ts
+++ src/controllers/dependencie.controller.ts
@@ -3,42 +3,52 @@ import { Request, Response } from 'express'
 import { connect } from '../database';
 import { Dependencie } from '../interfaces/Dependencie';
 
-export async function getDependencies(req: Request, res: Response): Promise<Response> {
-    const conn = await connect();
-    const dependencies = await conn.query('SELECT * FROM dependencies');
-    return res.json(dependencies[0]);
+class DependencieController{
+
+    constructor(){}
+
+    public async getDependencies(req: Request, res: Response): Promise<Response> {
+        const conn = await connect();
+        const dependencies = await conn.query('SELECT * FROM dependencies');
+        return res.json(dependencies[0]);
+    }
+    
+    public async createDependencie(req: Request, res: Response): Promise<Response>{
+        const newDependencie:Dependencie = req.body;
+        const conn = await connect();
+        await conn.query('INSERT INTO dependencies (name) VALUES (?)', [newDependencie.name])
+        return res.json({
+            message: 'Dependencie created'
+        });
+    }
+    
+    public async findDependencie(req:Request, res:Response): Promise<any> {
+        const id = req.params.dependencieId;
+        const conn = await connect();
+        const dependencie = await conn.query('SELECT * FROM dependencies WHERE id = ?', [id]);
+        return res.json(dependencie[0]);
+    }
+    
+    public async editDependencie(req:Request, res:Response): Promise<Response> {
+        const id = req.params.dependencieId;
+        const dependencie:Dependencie = req.body;
+        const conn = await connect();
+        await conn.query('UPDATE dependencies SET name = ? WHERE id = ?', [dependencie.name, id]);
+        return res.json({
+            message: 'Dependencie modificated'
+        });
+    }
+    
+    public async deleteDependencie(req:Request, res:Response): Promise<Response> {
+        const id = req.params.dependencieId;
+        const conn = await connect();
+        await conn.query('DELETE FROM dependencies WHERE id = ?', [id]);
+        return res.json({
+            message: 'Dependencie removed'
+        });
+    }
 }
 
-export async function createDependencie(req: Request, res: Response): Promise<Response>{
-    const newDependencie:Dependencie = req.body;
-    const conn = await connect();
-    await conn.query('INSERT INTO dependencies (name) VALUES (?)', [newDependencie.name])
-    return res.json({
-        message: 'Dependencie created'
-    });
-}
+const dependencieController = new DependencieController();
+export default dependencieController;
 
-export async function findDependencie(req:Request, res:Response): Promise<Response> {
-    const id = req.params.dependencieId;
-    const conn = await connect();
-    const dependencie = await conn.query('SELECT * FROM dependencies WHERE id = ?', [id]);
-    return res.json(dependencie[0]);
-}
-
-export async function editDependencie(req:Request, res:Response): Promise<Response> {
-    const dependencie:Dependencie = req.body;
-    const conn = await connect();
-    await conn.query('UPDATE dependencies SET name = ? WHERE id = ?', [dependencie.name, dependencie.id]);
-    return res.json({
-        message: 'Dependencie modificated'
-    });
-}
-
-export async function deleteDependencie(req:Request, res:Response): Promise<Response> {
-    const id = req.params.dependencieId;
-    const conn = await connect();
-    await conn.query('DELETE FROM dependencies WHERE id = ?', [id]);
-    return res.json({
-        message: 'Dependencie removed'
-    });
-}
--- src/routes/dependencie.routes.ts
+++ src/routes/dependencie.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 
-import { getDependencies, createDependencie, findDependencie, editDependencie, deleteDependencie } from '../controllers/dependencie.controller';
+import dependencieController from '../controllers/dependencie.controller';
 
 const router = Router();
 
 router.route('/')
-    .get(getDependencies)
-    .post(createDependencie);
+    .get(dependencieController.getDependencies)
+    .post(dependencieController.createDependencie);
 
 router.route('/:dependencieId')
-    .get(findDependencie)
-    .post(editDependencie)
-    .delete(deleteDependencie);
+    .get(dependencieController.findDependencie)
+    .put(dependencieController.editDependencie)
+    .delete(dependencieController.deleteDependencie);
 
 export default router;
