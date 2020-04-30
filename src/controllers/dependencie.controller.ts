3f3c5ef653a4ae3e23174339baf60aa9a6d82aef
Ruben Rodriguez <proyecto.rubendariorq@gmail.com>
2020-04-30 13:11:59 -0500

    refactor: Refactor the code

--- src/controllers/dependencie.controller.ts
+++ src/controllers/dependencie.controller.ts
@@ -3,49 +3,79 @@ import { Request, Response } from 'express'
 import { connect } from '../database';
 import { Dependencie } from '../interfaces/Dependencie';
 
-class DependencieController{
+class DependencieController {
 
-    constructor(){}
+    constructor() { }
 
-    public async getDependencies(req: Request, res: Response): Promise<Response> {
-        const conn = await connect();
-        const dependencies = await conn.query('SELECT * FROM dependencies');
-        return res.json(dependencies[0]);
+    public async getDependencies(req: Request, res: Response): Promise<any> {
+        try {
+            const conn = await connect();
+            const dependencies = await conn.query('SELECT * FROM dependencies');
+            conn.end();
+            return res.json(dependencies[0]);
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
     }
-    
-    public async createDependencie(req: Request, res: Response): Promise<Response>{
-        const newDependencie:Dependencie = req.body;
-        const conn = await connect();
-        await conn.query('INSERT INTO dependencies (name) VALUES (?)', [newDependencie.name])
-        return res.json({
-            message: 'Dependencie created'
-        });
+
+    public async createDependencie(req: Request, res: Response): Promise<any> {
+        const newDependencie: Dependencie = req.body;
+        try {
+            const conn = await connect();
+            await conn.query('INSERT INTO dependencies (name) VALUES (?)', [newDependencie.name]);
+            conn.end();
+            return res.json({
+                message: 'Dependencie created'
+            });
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
     }
-    
-    public async findDependencie(req:Request, res:Response): Promise<any> {
+
+    public async findDependencie(req: Request, res: Response): Promise<any> {
         const id = req.params.dependencieId;
-        const conn = await connect();
-        const dependencie = await conn.query('SELECT * FROM dependencies WHERE id = ?', [id]);
-        return res.json(dependencie[0]);
+        try {
+            const conn = await connect();
+            const dependencie = await conn.query('SELECT * FROM dependencies WHERE id = ?', [id]);
+            conn.end();
+            return res.json(dependencie[0]);
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
     }
-    
-    public async editDependencie(req:Request, res:Response): Promise<Response> {
+
+    public async editDependencie(req: Request, res: Response): Promise<any> {
         const id = req.params.dependencieId;
-        const dependencie:Dependencie = req.body;
-        const conn = await connect();
-        await conn.query('UPDATE dependencies SET name = ? WHERE id = ?', [dependencie.name, id]);
-        return res.json({
-            message: 'Dependencie modificated'
-        });
+        try {
+            const dependencie: Dependencie = req.body;
+            const conn = await connect();
+            await conn.query('UPDATE dependencies SET name = ? WHERE id = ?', [dependencie.name, id]);
+            conn.end();
+            return res.json({
+                message: 'Dependencie modificated'
+            });
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
     }
-    
-    public async deleteDependencie(req:Request, res:Response): Promise<Response> {
+
+    public async deleteDependencie(req: Request, res: Response): Promise<any> {
         const id = req.params.dependencieId;
-        const conn = await connect();
-        await conn.query('DELETE FROM dependencies WHERE id = ?', [id]);
-        return res.json({
-            message: 'Dependencie removed'
-        });
+        try {
+            const conn = await connect();
+            await conn.query('DELETE FROM dependencies WHERE id = ?', [id]);
+            conn.end();
+            return res.json({
+                message: 'Dependencie removed'
+            });
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
     }
 }
 
