19628bf3e9d6f83281b4c6a88f8c90ec422fb4f9
Ruben Rodriguez <proyecto.rubendariorq@gmail.com>
2020-05-06 17:26:09 -0500

    refactor: Refactor the code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,7 +11,6 @@ export class UserController {
         try {
             const conn = await connect();
             const users = await conn.query("select * from users u "
-                + "inner join employee_ti e on (u.email = e.users_email) "
                 + "inner join dependencies d on (u.dependencies_id = d.id);");
             conn.end();
             return res.json(users[0]);
@@ -28,13 +27,6 @@ export class UserController {
             await conn.query("INSERT INTO users "
                 + "(email, first_name, last_name, password, password_changed_date, type_user, status, failed_attempts, dependencies_id)"
                 + "VALUES (?,?,?,?,?,?,?,?,?)", [user.email, user.first_name, user.last_name, user.password, user.password_changed_date, user.type_user, user.status, user.failde_attempts, user.dependencies_id]);
-
-            if (user.type_user == "Funcionario") {
-                await conn.query("INSERT INTO employee (users_email) VALUES (?)", [user.email]);
-            } else {
-                await conn.query("INSERT INTO employee_ti (users_email, type_employee_ti) VALUES (?,?)", [user.email, user.type_employee_ti]);
-            }
-
             conn.end();
             return res.json({
                 message: 'User created'
