ae2aec42d0d913e4a0cf68c43336570845188132
Ruben Rodriguez <proyecto.rubendariorq@gmail.com>
2020-06-04 18:49:45 -0500

    feat: Check current hardware location

--- src/controllers/hardware.controller.ts
+++ src/controllers/hardware.controller.ts
@@ -14,9 +14,9 @@ class HardwareController {
         console.log(computer);
         try {
             const conn = await connect();
-            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [computer.inventory_plate, computer.serial, computer.cost, computer.months_warranty, computer.brand, computer.status, computer.buy_date, computer.provider, computer.model, computer.type_hardware]);
+            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, allocation_status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [computer.inventory_plate, computer.serial, computer.cost, computer.months_warranty, computer.brand, computer.allocation_status, computer.buy_date, computer.provider, computer.model, computer.type_hardware]);
 
-            await conn.query("INSERT INTO computers (type_computer, processor, speed_processor, hard_drive, technology_hard_drive, memory, type_memory, brand_monitor, model_monitor, inch_monitor, serial_monitor, brand_network_card, speed_network_card, drive, cd_rom, dvd, card_reader_driver, tape_backup, external_hard_drive, keyboard_connection, mouse_connection, observations, hardware_inventory_plate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", [computer.type_computer, computer.processor, computer.speed_processor, computer.hard_drive, computer.technology_hard_drive, computer.memory, computer.type_memory, computer.brand_monitor, computer.model_monitor, computer.inch_monitor, computer.serial_monitor, computer.brand_network_card, computer.speed_network_card, computer.drive, computer.cd_rom, computer.dvd, computer.card_reader_driver, computer.tape_backup, computer.external_hard_drive, computer.keyboard_connection, computer.mouse_connection, computer.observations, computer.hardware_inventory_plate]);
+            await conn.query("INSERT INTO computers (type_computer, processor, processor_unit_measure, speed_processor, hard_drive, hard_drive_unit_measure, technology_hard_drive, memory, memory_unit_measure, type_memory, brand_monitor, model_monitor, inch_monitor, serial_monitor, brand_network_card, speed_network_card, drive, cd_rom, dvd, card_reader_driver, tape_backup, external_hard_drive, keyboard_connection, mouse_connection, observations, hardware_inventory_plate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", [computer.type_computer, computer.processor, computer.processor_unit_measure, computer.speed_processor, computer.hard_drive, computer.hard_drive_unit_measure, computer.technology_hard_drive, computer.memory, computer.memory_unit_measure, computer.type_memory, computer.brand_monitor, computer.model_monitor, computer.inch_monitor, computer.serial_monitor, computer.brand_network_card, computer.speed_network_card, computer.drive, computer.cd_rom, computer.dvd, computer.card_reader_driver, computer.tape_backup, computer.external_hard_drive, computer.keyboard_connection, computer.mouse_connection, computer.observations, computer.hardware_inventory_plate]);
 
             conn.end();
             return res.json({
@@ -32,7 +32,7 @@ class HardwareController {
         let ups: Ups = req.body;
         try {
             const conn = await connect();
-            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [ups.inventory_plate, ups.serial, ups.cost, ups.months_warranty, ups.brand, ups.status, ups.buy_date, ups.provider, ups.model, ups.type_hardware]);
+            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, allocation_status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [ups.inventory_plate, ups.serial, ups.cost, ups.months_warranty, ups.brand, ups.allocation_status, ups.buy_date, ups.provider, ups.model, ups.type_hardware]);
 
             await conn.query("INSERT INTO ups (capacity, hardware_inventory_plate) VALUES (?,?);", [ups.capacity, ups.hardware_inventory_plate]);
 
@@ -50,7 +50,7 @@ class HardwareController {
         let peripheral: Peripheral = req.body;
         try {
             const conn = await connect();
-            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [peripheral.inventory_plate, peripheral.serial, peripheral.cost, peripheral.months_warranty, peripheral.brand, peripheral.status, peripheral.buy_date, peripheral.provider, peripheral.model, peripheral.type_hardware]);
+            await conn.query("INSERT INTO hardware (inventory_plate, serial, cost, months_warranty, brand, allocation_status, buy_date, provider, model, type_hardware) VALUES (?,?,?,?,?,?,?,?,?,?);", [peripheral.inventory_plate, peripheral.serial, peripheral.cost, peripheral.months_warranty, peripheral.brand, peripheral.allocation_status, peripheral.buy_date, peripheral.provider, peripheral.model, peripheral.type_hardware]);
 
             await conn.query("INSERT INTO peripherals (type_peripheral, hardware_inventory_plate) VALUES (?,?);", [peripheral.type_peripheral, peripheral.hardware_inventory_plate]);
 
@@ -67,27 +67,7 @@ class HardwareController {
     public async getAllHardware(req: Request, res: Response): Promise<any> {
         try {
             const conn = await connect();
-            const hardware = await conn.query("SELECT * FROM hardware h LEFT JOIN hardware_for_users hu " +
-            "ON (h.inventory_plate = hu.hardware_inventory_plate) LEFT JOIN users u " +
-            "ON (hu.users_id_user = u.id_user) LEFT JOIN dependencies d " + 
-            "ON (u.dependencies_id_dependencie = d.id_dependencie);");
-            conn.end();
-            return res.json(hardware[0]);
-        } catch (e) {
-            console.error(e);
-            return res.json(e);
-        }
-    }
-
-    public async getHardwareForDependencies(req: Request, res: Response): Promise<any> {
-        try {
-            const dependencie = req.params.dependencie;
-            const conn = await connect();
-            const hardware = await conn.query("SELECT * FROM hardware h LEFT JOIN hardware_for_users hu " +
-            "ON (h.inventory_plate = hu.hardware_inventory_plate) LEFT JOIN users u " +
-            "ON (hu.users_id_user = u.id_user) LEFT JOIN dependencies d " + 
-            "ON (u.dependencies_id_dependencie = d.id_dependencie) " +
-            "WHERE d.name_dependencie = ?;", [dependencie]);
+            const hardware = await conn.query("SELECT * FROM hardware ;");
             conn.end();
             return res.json(hardware[0]);
         } catch (e) {
@@ -105,6 +85,19 @@ class HardwareController {
             "ON (hu.users_id_user = u.id_user) LEFT JOIN dependencies d " + 
             "ON (u.dependencies_id_dependencie = d.id_dependencie) " +
             "WHERE h.type_hardware = ?;", [typeHardware]);
+            conn.end();
+            return res.json(hardware[0]);
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
+    }
+
+    public async getHardwareForAllocationStatus(req: Request, res: Response): Promise<any> {
+        try {
+            const allocationStatus = req.params.allocationStatus;
+            const conn = await connect();
+            const hardware = await conn.query("SELECT * FROM hardware WHERE allocation_status = ?;", [allocationStatus]);
             conn.end();
             return res.json(hardware[0]);
         } catch (e) {
@@ -134,9 +127,9 @@ class HardwareController {
         
         try {
             const conn = await connect();
-            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [computer.inventory_plate, computer.serial, computer.cost, computer.months_warranty, computer.brand, computer.status, computer.buy_date, computer.provider, computer.model, computer.type_hardware, inventoryPlate]);
+            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, allocation_status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [computer.inventory_plate, computer.serial, computer.cost, computer.months_warranty, computer.brand, computer.allocation_status, computer.buy_date, computer.provider, computer.model, computer.type_hardware, inventoryPlate]);
 
-            await conn.query("UPDATE computers SET type_computer = ?, processor = ?, speed_processor = ?, hard_drive = ?, technology_hard_drive = ?, memory = ?, type_memory = ?, brand_monitor = ?, model_monitor = ?, inch_monitor = ?, serial_monitor = ?, brand_network_card = ?, speed_network_card = ?, drive = ?, cd_rom = ?, dvd = ?, card_reader_driver = ?, tape_backup = ?, external_hard_drive = ?, keyboard_connection = ?, mouse_connection = ?, observations = ? WHERE hardware_inventory_plate = ?;", [computer.type_computer, computer.processor, computer.speed_processor, computer.hard_drive, computer.technology_hard_drive, computer.memory, computer.type_memory, computer.brand_monitor, computer.model_monitor, computer.inch_monitor, computer.serial_monitor, computer.brand_network_card, computer.speed_network_card, computer.drive, computer.cd_rom, computer.dvd, computer.card_reader_driver, computer.tape_backup, computer.external_hard_drive, computer.keyboard_connection, computer.mouse_connection, computer.observations, computer.hardware_inventory_plate]);
+            await conn.query("UPDATE computers SET type_computer = ?, processor = ?, processor_unit_measure = ?, speed_processor = ?, hard_drive = ?, hard_drive_unit_measure = ?, technology_hard_drive = ?, memory = ?, memory_unit_measure = ?, type_memory = ?, brand_monitor = ?, model_monitor = ?, inch_monitor = ?, serial_monitor = ?, brand_network_card = ?, speed_network_card = ?, drive = ?, cd_rom = ?, dvd = ?, card_reader_driver = ?, tape_backup = ?, external_hard_drive = ?, keyboard_connection = ?, mouse_connection = ?, observations = ? WHERE hardware_inventory_plate = ?;", [computer.type_computer, computer.processor, computer.processor_unit_measure, computer.speed_processor, computer.hard_drive, computer.hard_drive_unit_measure, computer.technology_hard_drive, computer.memory, computer.memory_unit_measure, computer.type_memory, computer.brand_monitor, computer.model_monitor, computer.inch_monitor, computer.serial_monitor, computer.brand_network_card, computer.speed_network_card, computer.drive, computer.cd_rom, computer.dvd, computer.card_reader_driver, computer.tape_backup, computer.external_hard_drive, computer.keyboard_connection, computer.mouse_connection, computer.observations, computer.hardware_inventory_plate]);
 
             conn.end();
             return res.json({
@@ -168,7 +161,7 @@ class HardwareController {
         let ups: Ups = req.body;
         try {
             const conn = await connect();
-            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [ups.inventory_plate, ups.serial, ups.cost, ups.months_warranty, ups.brand, ups.status, ups.buy_date, ups.provider, ups.model, ups.type_hardware, inventoryPlate]);
+            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, allocation_status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [ups.inventory_plate, ups.serial, ups.cost, ups.months_warranty, ups.brand, ups.allocation_status, ups.buy_date, ups.provider, ups.model, ups.type_hardware, inventoryPlate]);
 
             await conn.query("UPDATE ups SET capacity = ? WHERE hardware_inventory_plate = ?;", [ups.capacity, ups.hardware_inventory_plate]);
 
@@ -202,7 +195,7 @@ class HardwareController {
         let peripheral: Peripheral = req.body;
         try {
             const conn = await connect();
-            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [peripheral.inventory_plate, peripheral.serial, peripheral.cost, peripheral.months_warranty, peripheral.brand, peripheral.status, peripheral.buy_date, peripheral.provider, peripheral.model, peripheral.type_hardware, inventoryPlate]);
+            await conn.query("UPDATE hardware SET inventory_plate = ?, serial = ?, cost = ?, months_warranty = ?, brand = ?, allocation_status = ?, buy_date = ?, provider = ?, model = ?, type_hardware = ? WHERE inventory_plate = ?;", [peripheral.inventory_plate, peripheral.serial, peripheral.cost, peripheral.months_warranty, peripheral.brand, peripheral.allocation_status, peripheral.buy_date, peripheral.provider, peripheral.model, peripheral.type_hardware, inventoryPlate]);
 
             await conn.query("UPDATE peripherals SET type_peripheral = ? WHERE hardware_inventory_plate = ?;", [peripheral.type_peripheral, peripheral.hardware_inventory_plate]);
 
@@ -225,6 +218,20 @@ class HardwareController {
             return res.json({
                 message: 'Hardware removed'
             });
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
+    }
+
+    public async getUbication(req: Request, res: Response): Promise<any> {
+        try {
+            const id = req.params.id;
+            const conn = await connect();
+            const hardware = await conn.query("SELECT * FROM hardware_for_users hu INNER JOIN users u " +
+            "ON (hu.users_id_user = u.id_user) INNER JOIN dependencies d ON (u.dependencies_id_dependencie = d.id_dependencie) WHERE hardware_inventory_plate = ?;", [id]);
+            conn.end();
+            return res.json(hardware[0]);
         } catch (e) {
             console.error(e);
             return res.json(e);
---
+++ src/interfaces/HardwareUbications.ts
@@ -0,0 +1,18 @@
+export interface HardwareUbications {
+    users_id_user: number,
+    hardware_inventory_plate: string,
+    assignment_date: string,
+    return_date: string,
+    id_user: number,
+    email: string,
+    first_name: string,
+    last_name: string,
+    password: string,
+    password_changed_date: string,
+    type_user: string,
+    allocation_status: string,
+    failed_attempts: number,
+    dependencies_id_dependencie: number,
+    id_dependencie: number,
+    name_dependencie: string
+}
