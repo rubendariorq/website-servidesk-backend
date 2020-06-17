fc5cae8c4bced43c0ce99fb484c6142ed064832c
Ruben Rodriguez <proyecto.rubendariorq@gmail.com>
2020-06-17 16:49:43 -0500

    feat: Update software

--- src/controllers/software.controller.ts
+++ src/controllers/software.controller.ts
@@ -27,9 +27,37 @@ export class SoftwareController {
     public async getAllSoftware(req: Request, res: Response): Promise<any> {
         try {
             const conn = await connect();
-            const licenses = await conn.query("select * from software;");
+            const software = await conn.query("select * from software;");
             conn.end();
-            return res.json(licenses[0]);
+            return res.json(software[0]);
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
+    }
+
+    public async getSoftware(req: Request, res: Response): Promise<any> {
+        const id = req.params.id;
+        try {
+            const conn = await connect();
+            const software = await conn.query("select * from software WHERE id_software = ?;",[id]);
+            conn.end();
+            return res.json(software[0]);
+        } catch (e) {
+            console.error(e);
+            return res.json(e);
+        }
+    }
+
+    public async editSoftware(req: Request, res: Response): Promise<any> {
+        try {
+            const software: Software = req.body;
+            const conn = await connect();
+            await conn.query('UPDATE software SET name_software = ?, version = ?, status_software = ?, development_information = ?, support = ?, plataform = ?, development_language = ?, provider = ?, software_property = ?, code_property = ?, status_support = ?, cost_maintenance = ?, contractual_use_restriction = ? WHERE id_software = ?', [software.name_software, software.version, software.status_software, software.development_information, software.support, software.plataform, software.development_language, software.provider, software.software_property, software.code_property,software.status_support, software.cost_maintenance, software.contractual_use_restriction, software.id_software]);
+            conn.end();
+            return res.json({
+                message: 'Software modificated'
+            });
         } catch (e) {
             console.error(e);
             return res.json(e);
--- src/routes/software.routes.ts
+++ src/routes/software.routes.ts
@@ -5,6 +5,10 @@ const router = Router();
 
 router.route('/')
     .get(softwareController.getAllSoftware)
-    .post(softwareController.addSoftware);
+    .post(softwareController.addSoftware)
+    .put(softwareController.editSoftware);
+
+router.route('/:id')
+    .get(softwareController.getSoftware);
 
 export default router;
